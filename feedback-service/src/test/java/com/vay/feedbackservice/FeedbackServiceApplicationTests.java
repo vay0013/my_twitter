74e6694b6bb895480858e4eb33b59171e54ac014
vay <prop6151@gmail.com>
2024-11-07 12:41:09 +1000

    update

---
+++ feedback-service/src/test/java/com/vay/feedbackservice/FeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vay.feedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
